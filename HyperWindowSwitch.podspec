f94e137a3197b92a7be9e67e3dccbced689e520e
Hamidreza Vakilian <xerxes235@yahoo.com>
2018-01-21 15:14:14 +0330

    Release 0.1.5

--- HyperWindowSwitch.podspec
+++ HyperWindowSwitch.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HyperWindowSwitch'
-  s.version          = '0.1.3'
+  s.version          = '0.1.5'
   s.summary          = 'Easily Switch UIWindow’s RootviewController'
 
 # This description is used to generate tags and improve search results.
--- HyperWindowSwitch/Classes/HyperWindowSwitch.m
+++ HyperWindowSwitch/Classes/HyperWindowSwitch.m
@@ -10,6 +10,11 @@
 #import "HyperWindowSwitchDummyVC.h"
 #import "HyperWindowSwitchTransitioningDelegate.h"
 
+@protocol visibleStatusBarInterface <NSObject>
+
+@property(nonatomic, readonly) UIStatusBarStyle preferredStatusBarStyle;
+
+@end
 
 @implementation HyperWindowSwitch
 
@@ -22,7 +27,7 @@ __weak static id appDelegate;
         UIWindow *window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
         [theAppDelegate setWindow:window];
         [window makeKeyAndVisible];
-		appDelegate = theAppDelegate;
+        appDelegate = theAppDelegate;
     }
     else{
         NSLog(@"ERROR");
@@ -31,18 +36,18 @@ __weak static id appDelegate;
 
 
 +(void) setRootViewController:(UIViewController*) firstVC {
-	[[appDelegate window] setRootViewController:firstVC];
+    [[appDelegate window] setRootViewController:firstVC];
 }
 
 
 +(void)switchToViewController:(UIViewController*)newVC animationMode:(HyperWindowSwitchAnimationMode)animationMode completion:(void (^)())completion;
 {
-	if (![[appDelegate window] rootViewController])
-	{
-		[self setRootViewController:newVC];
-		return;
-	}
-	
+    if (![[appDelegate window] rootViewController])
+    {
+        [self setRootViewController:newVC];
+        return;
+    }
+    
     if (animationMode == HyperWindowSwitchAnimationModeNone)
     {
         UIViewController* currentVC = [[appDelegate window] rootViewController] ;
@@ -64,31 +69,48 @@ __weak static id appDelegate;
     }
     else if (animationMode == HyperWindowSwitchAnimationModeTransitioningDelegate)
     {
-		UIViewController* currentVC = [[appDelegate window] rootViewController] ;
-		[currentVC presentViewController:newVC animated:YES completion:^{
-
-			//now we have to dismiss currentVC and immediately set the window.rootViewController. but it will cause a slight glitch in status bar. to avoid it, we have to create a snapshot of the presented vc as the dummyVC and set window.rootViewController = dummyVC. Now we can safely tell the currentVC to dismiss and and it's completion we set the window.rootViewController to the real newVC. Note that the dummyVC is a subclass of HyperWindowSwitchDummyVC. HyperWindowSwitchDummyVC is just a simple view controller but it immitates newVC's status bar style.
-			HyperWindowSwitchDummyVC* vc = [HyperWindowSwitchDummyVC new];
-			vc.statusBarHidden = [newVC prefersStatusBarHidden];
-			vc.statusBarStyle = [newVC preferredStatusBarStyle];
-			[vc.view addSubview:[newVC.view snapshotViewAfterScreenUpdates:YES]];
-			[[appDelegate window] setRootViewController:vc];
-
-			[currentVC dismissViewControllerAnimated:NO completion:^{
-				[[appDelegate window] setRootViewController:newVC];
-				if (completion)
-					completion();
-			;}];
-		}];
+        
+        //*********** We wrap up the currentVC into a HyperWindowSwitchDummyVC ********//
+        // Reason: because the currentVC in some cases won't perform the animation delegate context immediately. (after a touch or some other ui event it will perform be performed! (without receiving any warnings or errors) terrible bug in iOS.) to make it work, it seems that if we wrap the current vc inside another uiviewcontroller it will be performed well.
+        HyperWindowSwitchDummyVC* dummyVC = [HyperWindowSwitchDummyVC new];
+        
+        // since preferredStatusBarStyle is not accessible outside the uiviewcontroller, we implemented this protocol
+        UIViewController<visibleStatusBarInterface>* currentVC = (UIViewController<visibleStatusBarInterface>*)[[appDelegate window] rootViewController];
+        
+        [dummyVC.view addSubview:currentVC.view];
+        [dummyVC addChildViewController:currentVC];
+        
+        dummyVC.statusBarStyle = currentVC.preferredStatusBarStyle;
+        
+        [[appDelegate window] setRootViewController:dummyVC];
+        
+        
+        [dummyVC presentViewController:newVC animated:YES completion:^{
+            
+            //now we have to dismiss currentVC and immediately set the window.rootViewController. but it will cause a slight glitch in status bar. to avoid it, we have to create a snapshot of the presented vc as the dummyVC and set window.rootViewController = dummyVC. Now we can safely tell the currentVC to dismiss and and it's completion we set the window.rootViewController to the real newVC. Note that the dummyVC is a subclass of HyperWindowSwitchDummyVC. HyperWindowSwitchDummyVC is just a simple view controller but it immitates newVC's status bar style.
+            HyperWindowSwitchDummyVC* vc = [HyperWindowSwitchDummyVC new];
+            vc.statusBarHidden = [newVC prefersStatusBarHidden];
+            vc.statusBarStyle = [newVC preferredStatusBarStyle];
+            [vc.view addSubview:[newVC.view snapshotViewAfterScreenUpdates:YES]];
+            [[appDelegate window] setRootViewController:vc];
+            
+            [currentVC dismissViewControllerAnimated:NO completion:^{
+                [[appDelegate window] setRootViewController:newVC];
+                if (completion)
+                    completion();
+                ;}];
+        }];
     }
     else {
         HyperWindowSwitchTransitioningDelegate *transitioningDelegate = [HyperWindowSwitchTransitioningDelegate new];
         transitioningDelegate.animationMode = animationMode;
-
+        
         newVC.transitioningDelegate = transitioningDelegate;
         newVC.modalPresentationStyle = UIModalPresentationCustom;
-
+        
         UIViewController* currentVC = [[appDelegate window] rootViewController] ;
+        
+        [newVC setNeedsStatusBarAppearanceUpdate];
         [currentVC presentViewController:newVC animated:YES completion:^{
             
             //now we have to dismiss currentVC and immediately set the window.rootViewController. but it will cause a slight glitch in status bar. to avoid it, we have to create a snapshot of the presented vc as the dummyVC and set window.rootViewController = dummyVC. Now we can safely tell the currentVC to dismiss and and it's completion we set the window.rootViewController to the real newVC. Note that the dummyVC is a subclass of HyperWindowSwitchDummyVC. HyperWindowSwitchDummyVC is just a simple view controller but it immitates newVC's status bar style.
@@ -97,7 +119,7 @@ __weak static id appDelegate;
             vc.statusBarStyle = [newVC preferredStatusBarStyle];
             [vc.view addSubview:[newVC.view snapshotViewAfterScreenUpdates:YES]];
             [[appDelegate window] setRootViewController:vc];
-
+            
             [currentVC dismissViewControllerAnimated:NO completion:^{
                 [[appDelegate window] setRootViewController:newVC];
                 if (completion)
@@ -108,3 +130,4 @@ __weak static id appDelegate;
 }
 
 @end
+
